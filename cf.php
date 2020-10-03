ce69c58a1d7b62eb00e8f698c5ff1178e88460ec
whysa12000 <72296557+whysa12000@users.noreply.github.com>
2020-10-03 15:00:56 +0700

    bzz

---
+++ cf.php
@@ -0,0 +1,12 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$email-message = $_POST['email-message'];
+$formcontent=" From: $name \n Email : $email \n Message: $email-message";
+$recipient = "iaaslcunej@gmail.com";
+$subject = "Kontak Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "<meta http-equiv=\"refresh\" content=\"0; url=index.php\">";
+?>
+
