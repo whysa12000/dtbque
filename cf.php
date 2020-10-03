c2559c9fac5f81a6a0bf835f59197fddc61aabc7
whysa12000 <72296557+whysa12000@users.noreply.github.com>
2020-10-03 15:05:04 +0700

    Update cf.php

--- cf.php
+++ cf.php
@@ -4,7 +4,7 @@ $email = $_POST['email'];
 $email-message = $_POST['email-message'];
 $formcontent=" From: $name \n Email : $email \n Message: $email-message";
 $recipient = "iaaslcunej@gmail.com";
-$subject = "Kontak Form";
+$subject = "contact";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "<meta http-equiv=\"refresh\" content=\"0; url=index.php\">";
